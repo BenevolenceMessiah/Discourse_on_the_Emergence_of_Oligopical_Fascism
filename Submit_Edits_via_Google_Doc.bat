40c9356ac0dff57b8091aa613757be04cde14fae
BenevolenceMessiah <120072200+BenevolenceMessiah@users.noreply.github.com>
2023-08-05 06:13:20 -0400

    Add files via upload

---
+++ Submit_Edits_via_Google_Doc.bat
@@ -0,0 +1,3 @@
+# Use this to open the Google Doc for collaborative editing. Credit yourself accordingly. Commits/Edits in the Google Doc that are chosen as contributions to the theory will be added to the Main Branch Repo (https://github.com/BenevolenceMessiah/Discourse_on_the_Emergence_of_Oligopical_Fascism).
+
+start https://docs.google.com/document/d/1pfrm2rXGjD_Q9w69lCzttVe23QhjjlMoBPEY_651Srk/edit?usp=sharing
